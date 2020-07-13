88c51f90cd9906da05785b76425ed57b787402e6
zarif14-58 <a.jevan14nzs@gmail.com>
2020-07-13 21:39:22 +0600

    Attemp to fix deployment issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/headerComp'
-import Main from './components/MainComp';
+import Main from './components/mainComp';
 
 function App() {
   return (
